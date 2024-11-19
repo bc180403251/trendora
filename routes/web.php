77a0b7caad49b9b04b40ab877f3b0531f86f4593
Mohsan <amohsan12345678@gmail.com>
2024-11-19 21:32:26 +0500

    frontend designs completed

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,16 @@ use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\HomePage;
 use App\Livewire\ProductsPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\MyOrderPage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\SuccessPage;
+use App\Livewire\CancelPage;
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
+use App\Livewire\Auth\ForgotPassword;
+use App\Livewire\Auth\ResetPassword;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +28,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/login', Login::class);
+Route::get('/register', Register::class);
+Route::get('/forgot', ForgotPassword::class);
+Route::get('/reset', ResetPassword::class);
+
 Route::get('/', HomePage::class);
 Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/cart',CartPage::class);
+Route::get('/products/{product}', ProductDetailPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MyOrderPage::class);
+Route::get('/my-orders/{order}', MyOrderDetailPage::class);
+
+Route::get('/succes', SuccessPage::class);
+Route::get('/cancel', CancelPage::class);
